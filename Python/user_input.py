a5803ec0cf940cee32982007551a979f1d1b7d02
colspan <collin@collindigm.com>
2025-02-16 13:47:02 +0300

    Repository Clean-up

--- Python/user_input.py
+++
@@ -1,5 +0,0 @@
-name = input("Enter name: ");
-age = input("Enter age: ");
-location = input("Enter location: ");
-
-print("Hello {}, you are {} years old and live in {}.".format(name, age, location))
